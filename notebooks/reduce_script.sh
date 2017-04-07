33e7671d861687e8c31a17e44fd8c6d4a088ca67
Maximilian H Abitbol <mabitbol15@gmail.com>
2017-04-07 12:02:43 -0400

    name

---
+++ notebooks/reduce_script.sh
@@ -0,0 +1,35 @@
+source activate gbt
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_21.raw.vegas/scan_02_21.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_19.raw.vegas/scan_02_19.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_39.raw.vegas/scan_02_39.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_25.raw.vegas/scan_02_25.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan46.raw.vegas/scan46.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_37.raw.vegas/scan_02_37.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_20.raw.vegas/scan_02_20.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan47.raw.vegas/scan47.raw.vegas.B.fits &
+wait
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_28.raw.vegas/scan_02_28.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_29.raw.vegas/scan_02_29.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_30.raw.vegas/scan_02_30.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_23.raw.vegas/scan_02_23.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_38.raw.vegas/scan_02_38.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_24.raw.vegas/scan_02_24.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan41.raw.vegas/scan41.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_41.raw.vegas/scan_02_41.raw.vegas.B.fits &
+wait
+python reduce_scan.py /data2/GBT/S140/Daisy/scan45.raw.vegas/scan45.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_32.raw.vegas/scan_02_32.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_34.raw.vegas/scan_02_34.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_40.raw.vegas/scan_02_40.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_18.raw.vegas/scan_02_18.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_42.raw.vegas/scan_02_42.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_35.raw.vegas/scan_02_35.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan44.raw.vegas/scan44.raw.vegas.B.fits &
+wait
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_17.raw.vegas/scan_02_17.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_36.raw.vegas/scan_02_36.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_26.raw.vegas/scan_02_26.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan42.raw.vegas/scan42.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_33.raw.vegas/scan_02_33.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_22.raw.vegas/scan_02_22.raw.vegas.B.fits &
+python reduce_scan.py /data2/GBT/S140/Daisy/scan_02_31.raw.vegas/scan_02_31.raw.vegas.B.fits &
